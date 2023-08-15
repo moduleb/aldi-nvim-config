4094e2d00488f217d4d7e467c308f51598bf1ef6
Denis <alkin.denis@gmail.com>
2023-08-15 11:44:36 +0400

    style: apply luaformatter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,28 +1,26 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require("lsp-zero").preset({})
 
 lsp.on_attach(function(_, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({ buffer = bufnr })
 end)
 
 -- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
 
-
 local cmp = require("cmp")
 cmp.setup({
-    sources = {
-        {name = "nvim_lsp"},
-	{name = "buffer"},
-	{name = "path"},
-	{name = "luasnip"},
-    },
-    mapping = {
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
-
-    }
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "buffer" },
+		{ name = "path" },
+		{ name = "luasnip" },
+	},
+	mapping = {
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-Space>"] = cmp.mapping.complete(),
+	},
 })
