fe06978ba4340b0afb882cbbb5962724632edb9a
Denis <alkin.denis@gmail.com>
2023-08-15 11:45:12 +0400

    refactor: change formatter settings

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,29 +1,51 @@
+local util = require("formatter.util")
+
+local function copyf(f)
+	return function(...)
+		return f(...)
+	end
+end
+
+local prettierFormat = function(parser)
+	if not parser then
+		return {
+			exe = "prettier",
+			args = {
+				"--stdin-filepath",
+				util.escape_path(util.get_current_buffer_file_path()),
+				"--tab-width",
+				"4",
+			},
+			stdin = true,
+			try_node_modules = true,
+		}
+	end
+
+	return {
+		exe = "prettier",
+		args = {
+			"--stdin-filepath",
+			util.escape_path(util.get_current_buffer_file_path()),
+			"--tab-width",
+			"4",
+			"--parser",
+			parser,
+		},
+		stdin = true,
+		try_node_modules = true,
+	}
+end
+
 require("formatter").setup({
 	logging = false,
 	filetype = {
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
-		python = {
-			require("formatter.filetypes.python").black,
-		},
-		javascript = {
-			require("formatter.filetypes.javascript").prettierd,
-		},
-		javascriptreact = {
-			require("formatter.filetypes.javascript").prettierd,
-		},
-		typescript = {
-			require("formatter.filetypes.javascript").prettierd,
-		},
-		typescriptreact = {
-			require("formatter.filetypes.javascript").prettierd,
-		},
-		html = {
-			require("formatter.filetypes.javascript").prettierd,
-		},
-		css = {
-			require("formatter.filetypes.javascript").prettierd,
-		},
+		lua = { require("formatter.filetypes.lua").stylua },
+		python = { require("formatter.filetypes.python").black },
+		typescript = { prettierFormat("typescript") },
+		typescriptreact = { prettierFormat("typescriptreact") },
+		javascript = { copyf(prettierFormat) },
+		javascriptreact = { copyf(prettierFormat) },
+		html = { copyf(prettierFormat) },
+		css = { prettierFormat("css") },
 	},
 })
