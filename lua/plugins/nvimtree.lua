5a4fe6c9a2c9b5feea7514d028102e691d029da2
Denis <alkin.denis@gmail.com>
2023-08-18 19:05:02 +0400

    feat: окно с деревом проекта открывает справа

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -6,81 +6,84 @@ vim.g.loaded_netrwPlugin = 1
 -- vim.opt.termguicolors = true
 
 local function my_on_attach(bufnr)
-  local api = require "nvim-tree.api"
+	local api = require("nvim-tree.api")
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
 
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
 
-  -- custom mappings
-  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
-  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-  vim.keymap.set('n', '<leader>t', vim.cmd.NvimTreeToggle, {})
-  vim.keymap.set('n', '<leader>e', vim.cmd.NvimTreeFocus, {})
-  vim.keymap.set('n', '<leader>ft', vim.cmd.NvimTreeFindFile, {})
+	-- custom mappings
+	vim.keymap.set("n", "<C-t>", api.tree.change_root_to_parent, opts("Up"))
+	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
+	vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle, {})
+	vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus, {})
+	vim.keymap.set("n", "<leader>ft", vim.cmd.NvimTreeFindFile, {})
 end
 
-local HEIGHT_RATIO = 0.8 -- You can change this
-local WIDTH_RATIO = 0.5  -- You can change this too
-
+-- local HEIGHT_RATIO = 0.8 -- You can change this
+-- local WIDTH_RATIO = 0.5  -- You can change this too
 
 require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    disable_netrw = true,
-    hijack_netrw = true,
-    respect_buf_cwd = true,
-    sync_root_with_cwd = true,
-    view = {
-        relativenumber = true,
-        float = {
-            enable = true,
-            open_win_config = function()
-                local screen_w = vim.opt.columns:get()
-                local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-                local window_w = screen_w * WIDTH_RATIO
-                local window_h = screen_h * HEIGHT_RATIO
-                local window_w_int = math.floor(window_w)
-                local window_h_int = math.floor(window_h)
-                local center_x = (screen_w - window_w) / 2
-                local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                                 - vim.opt.cmdheight:get()
-                return {
-                  border = "rounded",
-                  relative = "editor",
-                  row = center_y,
-                  col = center_x,
-                  width = window_w_int,
-                  height = window_h_int,
-                }
-                end,
-        },
-        width = function()
-          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-        end,
-    },
-    -- view = {
-    --     adaptive_size = true,
-    --     mappings = {
-    --         list = {
-    --             { key = "u", action = "dir_up" },
-    --         },
-    --     },
-    -- },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = false,
-    },
-    git = {
-        ignore = false,
-    },
-    on_attach = my_on_attach,
-})
+	sort_by = "case_sensitive",
+	disable_netrw = true,
+	hijack_netrw = true,
+	respect_buf_cwd = true,
+	sync_root_with_cwd = true,
 
+	-- window in center ->
+	-- view = {
+	--     relativenumber = true,
+	--     float = {
+	--         enable = true,
+	--         open_win_config = function()
+	--             local screen_w = vim.opt.columns:get()
+	--             local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+	--             local window_w = screen_w * WIDTH_RATIO
+	--             local window_h = screen_h * HEIGHT_RATIO
+	--             local window_w_int = math.floor(window_w)
+	--             local window_h_int = math.floor(window_h)
+	--             local center_x = (screen_w - window_w) / 2
+	--             local center_y = ((vim.opt.lines:get() - window_h) / 2)
+	--                              - vim.opt.cmdheight:get()
+	--             return {
+	--               border = "rounded",
+	--               relative = "editor",
+	--               row = center_y,
+	--               col = center_x,
+	--               width = window_w_int,
+	--               height = window_h_int,
+	--             }
+	--             end,
+	--     },
+	--     width = function()
+	--       return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+	--     end,
+	-- },
+
+	-- windown on right ->
+	view = {
+		relativenumber = true,
+		adaptive_size = true,
+		mappings = {
+			list = {
+				{ key = "u", action = "dir_up" },
+			},
+		},
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = false,
+	},
+	git = {
+		ignore = false,
+	},
+	on_attach = my_on_attach,
+})
 
 -- vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>")
 -- vim.keymap.set("n", "<leader>e", ":NvimTreeFocus<CR>")
