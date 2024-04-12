1fe0a4c9c512cb3d6c4ceccd933ed12d1fc7d8ab
aldeig <alkin.denis@gmail.com>
2024-04-12 10:31:29 +0400

    feat: added fold settings

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -15,6 +15,19 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
+-- Fold Settings
+vim.opt.fillchars = { fold = " " }
+vim.opt.foldmethod = "indent"
+vim.opt.foldenable = false
+vim.opt.foldlevel = 99
+vim.g.markdown_folding = 1 -- enable markdown folding
+
+-- with treesitter
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.cmd([[ set nofoldenable]])
+-- vim.opt.foldlevel = 99
+
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "88"
 
@@ -41,3 +54,17 @@ vim.api.nvim_create_autocmd(
     "BufEnter",
     { callback = indent.set_tabulation }
 )
+
+-- Fold Settings variants
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   callback = function()
+--     if require("nvim-treesitter.parsers").has_parser() then
+--       vim.opt.foldmethod = "expr"
+--       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+--     else
+--       vim.opt.foldmethod = "indent"
+--     end
+--   end,
+-- })
+-- vim.opt.set("nofoldenable")
+
