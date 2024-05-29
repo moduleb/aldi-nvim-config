ea18aaab015667fc183063e0802e2ab87265ecaf
aldeig <alkin.denis@gmail.com>
2024-05-29 12:01:56 +0400

    refactor: udpate setup

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,31 +1,27 @@
-require("bufferline").setup({
+local bufferline = require("bufferline")
+bufferline.setup({
     options = {
         diagnostics = "nvim_lsp",
-        separator_style = "padded_slope",
-        offsets = {
-            -- {
-            --   filetype = "NvimTree",
-            --   text = function()
-            --     return vim.fn.getcwd()
-            --   end,
-            --   highlight = "Directory",
-            --   text_align = "left"
-            -- }
-            {
-                filetype = "NvimTree",
-                text = "File Explorer",
-                text_align = "center", -- | "left" | "right"
-                separator = true
-            },
-        },
+        diagnostics_indicator = function(count, level)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon .. count
+        end,
+        -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        --     return "(" .. count .. ")"
+        -- end,
+        separator_style = "slant",
+        style_preset = bufferline.style_preset.italic,
         hover = {
             enabled = true,
             delay = 200,
-            reveal = {'close'}
+            reveal = { 'close' }
         },
         indicator = {
             icon = '▎', -- this should be omitted if indicator style is not 'icon'
             style = 'icon' -- | 'underline' | 'none',
         },
+        numbers = function(opts)
+            return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
+        end,
     },
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,13 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "lua", "python", "javascript", "html" },
-    sync_install = false,
-    auto_install = true,
-    highlight = { enable = true },
+    ensure_installed = { "lua", "printf", "luadoc", "vimdoc", "python", "javascript" },
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    },
+
     indent = { enable = true },
+    -- sync_install = false,
+    -- auto_install = true,
+    -- highlight = { enable = true },
+    -- indent = { enable = true },
 })
