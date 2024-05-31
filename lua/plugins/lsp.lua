a4e583dab27a14f78302ca797767f9f00b7b1e72
aldeig <alkin.denis@gmail.com>
2024-05-31 13:48:38 +0400

    feat: settings pyright and ruff-lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,67 +1,56 @@
--- local lsp = require("lsp-zero").preset({})
---
--- lsp.on_attach(function(_, bufnr)
--- 	-- see :help lsp-zero-keybindings
--- 	-- to learn the available actions
--- 	lsp.default_keymaps({ buffer = bufnr })
--- end)
---
--- -- (Optional) setup all installed_servers
--- -- require("lspinstall").setup()
--- -- local servers = require("lspinstall").installed_servers()
--- -- for _, server in pairs(servers) do
--- -- 	require("lspconfig")[server].setup({})
--- -- end
---
--- -- (Optional) Configure lua language server for neovim
--- require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
---
--- lsp.setup()
---
--- local cmp = require("cmp")
--- cmp.setup({
--- 	sources = {
--- 		{ name = "nvim_lsp" },
--- 		{ name = "buffer" },
--- 		{ name = "path" },
--- 		{ name = "luasnip" },
--- 	},
--- 	mapping = {
--- 		["<CR>"] = cmp.mapping.confirm({ select = true }),
--- 		["<C-Space>"] = cmp.mapping.complete(),
--- 	},
--- })
---
+local pyright_settings = require("plugins.pyright")
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+lsp_zero.on_attach(function(_, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+    ensure_installed = {},
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    }
 })
 
 local cmp = require('cmp')
 local cmp_format = lsp_zero.cmp_format()
 
 cmp.setup({
-  formatting = cmp_format,
-  mapping = cmp.mapping.preset.insert({
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    -- scroll up and down the documentation window
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
+    formatting = cmp_format,
+    mapping = cmp.mapping.preset.insert({
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        -- scroll up and down the documentation window
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    }),
 })
+
+
+local lspconfig = require('lspconfig')
+lspconfig.ruff_lsp.setup {
+    init_options = {
+        settings = {
+            -- Any extra CLI arguments for `ruff` go here.
+            args = {
+                -- "--select=PL,D,C4,E,G,W,F,B,SIM,UP,N,I,ASYNC,S,PTH,ARG,RUF",
+                "--ignore=ANN201,ANN003,COM812,CPY,D100,D102,D103,D107,D205,ERA001,RET502,RUF001,RUF002,RUF003",
+                "--select=ALL",
+                "--line-length=88",
+                "--preview",
+                "--respect-gitignore", -- Исключать из сканирования файлы в .gitignore
+                "--target-version=py312"
+            },
+        }
+    }
+}
+lspconfig.pyright.setup {
+    settings = pyright_settings
+}
