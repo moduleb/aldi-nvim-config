a1f79f8b48fee50bc38249cc9895d2df7766b4b2
aldeig <alkin.denis@gmail.com>
2024-07-18 13:51:52 +0400

    feat: change colorscheme

--- lua/core/colors.lua
+++ lua/core/colors.lua
@@ -56,13 +56,13 @@ end
 
 ------------------------------ CATPPUCCIN ------------------------------
 require("catppuccin").setup({
-    flavour = "mocha",     -- latte, frappe, macchiato, mocha
+    flavour = "frappe",     -- latte, frappe, macchiato, mocha
     transparent_background = true, -- disables setting the background color.
-    -- dim_inactive = {
-    --     enabled = false,   -- dims the background color of inactive window
-    --     shade = "dark",
-    --     percentage = 0.15, -- percentage of the shade to apply to the inactive window
-    -- },
+    dim_inactive = {
+        enabled = false,   -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
 })
 
 ColorMyPencils("catppuccin")
