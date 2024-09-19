0718d4cdc84c42104616092428104087b480fbd8
a <a@as-MacBook-Pro.local>
2024-09-19 14:41:33 +0400

    добавил whichkey

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -1,70 +1,84 @@
+-- Импортируем модуль для настройки отступов
 local indent = require("core.indent")
 
+-- Устанавливаем кодировку файла в UTF-8
 vim.opt.encoding = "utf-8"
+-- Отключаем автоматический перенос строк
 vim.opt.wrap = false
 
+-- Отключаем создание временных файлов
 vim.opt.swapfile = false
+-- Отключаем создание резервных копий
 vim.opt.backup = false
+-- Устанавливаем директорию для хранения файлов отмены
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+-- Включаем поддержку файлов отмены
 vim.opt.undofile = true
 
+-- Включаем подсветку результатов поиска
 vim.opt.hlsearch = true
+-- Включаем инкрементальный поиск
 vim.opt.incsearch = true
 
+-- Устанавливаем количество строк, которые будут видны при прокрутке
 vim.opt.scrolloff = 8
+-- Включаем отображение колонки знаков
 vim.opt.signcolumn = "yes"
+-- Добавляем символы к имени файла
 vim.opt.isfname:append("@-@")
 
--- Fold Settings
-vim.opt.fillchars = { fold = " " }
-vim.opt.foldmethod = "indent"
-vim.opt.foldenable = false
-vim.opt.foldlevel = 99
-vim.g.markdown_folding = 1 -- enable markdown folding
+-- Настройки сворачивания (folding)
+vim.opt.fillchars = { fold = " " } -- Устанавливаем символы для сворачивания
+vim.opt.foldmethod = "indent" -- Метод сворачивания по отступам
+vim.opt.foldenable = false -- Отключаем сворачивание по умолчанию
+vim.opt.foldlevel = 99 -- Устанавливаем уровень сворачивания
+vim.g.markdown_folding = 1 -- Включаем сворачивание для Markdown
 
--- with treesitter
--- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- vim.cmd([[ set nofoldenable]])
--- vim.opt.foldlevel = 99
+-- Настройки сворачивания с использованием Treesitter
+-- vim.opt.foldmethod = "expr" -- Метод сворачивания по выражению
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- Выражение для сворачивания
+-- vim.cmd([[ set nofoldenable]]) -- Отключаем сворачивание
+-- vim.opt.foldlevel = 99 -- Устанавливаем уровень сворачивания
 
+-- Устанавливаем время обновления в миллисекундах
 vim.opt.updatetime = 50
+-- Устанавливаем цветовую колонку на 88-й символ
 vim.opt.colorcolumn = "88"
 
--- Indent Settings
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.smartindent = true
+-- Настройки отступов
+vim.opt.expandtab = true -- Использовать пробелы вместо табуляции
+vim.opt.shiftwidth = 4 -- Количество пробелов для отступа
+vim.opt.tabstop = 4 -- Количество пробелов, соответствующее табуляции
+vim.opt.softtabstop = 4 -- Количество пробелов для табуляции в режиме вставки
+vim.opt.smartindent = true -- Включаем интеллектуальные отступы
 
--- Line Numbers
-vim.opt.nu = true
-vim.opt.relativenumber = true
+-- Настройки отображения номеров строк
+vim.opt.nu = true -- Включаем отображение номеров строк
+vim.opt.relativenumber = true -- Включаем относительные номера строк
 
--- Mouse
-vim.opt.mouse = "a"
-vim.opt.mousefocus = true
+-- Настройки мыши
+vim.opt.mouse = "a" -- Включаем поддержку мыши во всех режимах
+vim.opt.mousefocus = true -- Включаем фокусировку мыши
 
--- colors
-vim.opt.termguicolors = true
+-- Настройки цветов
+vim.opt.termguicolors = true -- Включаем поддержку 24-битных цветов
 
--- установка табуляции для разных типов файлов
+-- Установка табуляции для разных типов файлов
 vim.api.nvim_create_autocmd(
     "BufEnter",
-    { callback = indent.set_tabulation }
+    { callback = indent.set_tabulation } -- Вызываем функцию для установки табуляции при входе в буфер
 )
 
--- Fold Settings variants
+-- Варианты настроек сворачивания
 -- vim.api.nvim_create_autocmd({ "FileType" }, {
 --   callback = function()
 --     if require("nvim-treesitter.parsers").has_parser() then
---       vim.opt.foldmethod = "expr"
---       vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+--       vim.opt.foldmethod = "expr" -- Устанавливаем метод сворачивания по выражению
+--       vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- Устанавливаем выражение для сворачивания
 --     else
---       vim.opt.foldmethod = "indent"
+--       vim.opt.foldmethod = "indent" -- Если парсер не найден, используем метод по отступам
 --     end
 --   end,
 -- })
--- vim.opt.set("nofoldenable")
+-- vim.opt.set("nofoldenable") -- Отключаем сворачивание по умолчанию
 
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,6 +13,26 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
+    --------------------------------- WHICH KEY -----------------------------------
+    {
+      "folke/which-key.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      keys = {
+        {
+          "<leader>?",
+         function()
+            require("which-key").show({ global = false })
+          end,
+          desc = "Buffer Local Keymaps (which-key)",
+        },
+      },
+    },
+
     ------------------------------ WORK WITH FILES ------------------------------
     {
         "nvim-neo-tree/neo-tree.nvim",
